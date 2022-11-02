5f22f48c2a1e409bf0ffc9053bac8a9473d82d80
Mr Rehan <r8499989@gmail.com>
2022-11-02 16:29:39 +0530

    created a prime file

---
+++ prime.js
@@ -0,0 +1,17 @@
+
+let count=0;
+for (let i=i; i<Number; i++)
+{
+    if(Number%i==0)
+    {
+        count++;
+    }
+}
+if(count==2)
+{
+    console.log("prime")
+}
+else
+{
+    console.log("not prime")
+}
